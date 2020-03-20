996efe6c3b4266a2c297174343f28a4ccfb517ce
Harshith Gowda N <54528303+imharshith@users.noreply.github.com>
2020-03-20 16:10:42 +0530

    Create eeeanalysis.py

---
+++ eee/eeeanalysis.py
@@ -0,0 +1,27 @@
+#BIT EEE department placement analysis.
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+%matplotlib inline
+
+style.use('ggplot')
+
+YEAR=['2015','2016','2017','2018']
+EEE_OFFERS=[53,43,28,37] 
+EEE_ELIGIBLE=[49,48,47,47] 
+EEE_PLACED=[43,33,26,34]
+
+xpos=np.arange(len(YEAR))
+#print(xpos)
+
+plt.xticks(xpos,YEAR)
+plt.title("EEE DEPARTMENT")
+plt.xlabel("YEAR")
+plt.ylabel("NO_OF_STUDENTS")
+
+plt.bar(xpos-0.2,EEE_OFFERS,color='teal',width=0.2,label="offers")
+plt.bar(xpos,EEE_ELIGIBLE,color='tan',width=0.2,label="eligible")
+plt.bar(xpos+0.2,EEE_PLACED,color='orange',width=0.2,label="placed")
+plt.legend()
+plt.show()
